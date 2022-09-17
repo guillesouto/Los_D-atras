87a0a5417ae42cab79885eef954c9384bb7c91c7
209 <209@academicos.cesde.edu.co>
2022-09-17 13:03:14 -0500

    NTS7 se crea el ciclo while para el menu

---
+++ Principal.py
@@ -0,0 +1,16 @@
+print("***MENU***")
+print("1. saludamos")
+print("2. despedida")
+print("3. SALIR")
+opcion=100
+
+while(opcion != 3):
+    
+    if(opcion == 1):
+       print
+    elif(opcion ==2):
+       print
+    else:
+        print("digite una opcion valida")
+else:
+    print('fin')
